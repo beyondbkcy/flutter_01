b9e329e727fe55ee39061fedbed89fe177f795fc
2930118234 <2930118234@qq.com>
2019-05-23 19:53:54 +0800

    小球定位的算法

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import './utils/System.dart';
 
@@ -6,125 +8,159 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemUtils.setStatusBarStyle(Brightness.light);
+    SystemUtils.setStatusBarStyle(Brightness.dark);
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       title: '网易云音乐',
-      home: buildHome(),
+      home: Home(),
     );
   }
+}
 
 //app主体
-  Scaffold buildHome() => Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.topLeft,
-                margin: EdgeInsets.only(top: 24, left: 24),
-                child: Text(
-                  '热门搜索',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                  ),
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffeeeeee),
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            //底部栏
+            Positioned(
+              bottom: 0,
+              width: MediaQuery.of(context).size.width,
+              height: 56,
+              child: Container(
+                color: Colors.white,
+              ),
+            ),
+            //小球
+            Positioned(
+              bottom:32,
+              right: 28,
+              child: Container(
+                width: 48,
+                height: 48,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(48),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      offset: Offset(0, 20),
+                      blurRadius: 28,
+                    )
+                  ]
                 ),
               ),
-              buildHotSearch(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.topLeft,
-                    margin: EdgeInsets.only(left: 24),
-                    child: Text(
-                      '历史搜索',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 24),
-                    child: Icon(
-                      Icons.delete,
-                      color: Colors.white,
-                      size: 24,
-                    ),
-                  )
-                ],
-              ),
-              buildHistoricalSearch()
-            ],
-          ),
+            )
+            // Container(
+            //   alignment: Alignment.topLeft,
+            //   margin: EdgeInsets.only(top: 24, left: 24),
+            //   child: Text(
+            //     '热门搜索',
+            //     style: TextStyle(
+            //       color: Color(0xff1E1F2D),
+            //       fontSize: 32,
+            //       fontWeight: FontWeight.bold,
+            //     ),
+            //   ),
+            // ),
+            // buildHotSearch(),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: <Widget>[
+            //     Container(
+            //       alignment: Alignment.topLeft,
+            //       margin: EdgeInsets.only(left: 24, top: 24),
+            //       child: Text(
+            //         '历史搜索',
+            //         style: TextStyle(
+            //           color: Color(0xff1E1F2D),
+            //           fontSize: 16,
+            //           fontWeight: FontWeight.bold,
+            //         ),
+            //       ),
+            //     ),
+            //     Container(
+            //       margin: EdgeInsets.only(right: 24, top: 24),
+            //       child: Icon(
+            //         Icons.delete,
+            //         color: Color(0xff1E1F2D),
+            //         size: 24,
+            //       ),
+            //     )
+            //   ],
+            // ),
+            // buildHistoricalSearch(),
+          ],
         ),
-      );
+      ),
+    );
+  }
+}
 
 // 热门搜索
-  Container buildHotSearch() => Container(
-        padding: EdgeInsets.only(left: 0, top: 16),
-        child: Wrap(
-          alignment: WrapAlignment.start,
-          children: <Widget>[
-            buildContainer1('孤单心事'),
-            buildContainer1('青春住了谁'),
-            buildContainer1('Taylor Swift'),
-            buildContainer1('袁娅维远方韵律'),
-            buildContainer1('复仇者联盟'),
-            buildContainer1('bad guy'),
-          ],
-        ),
-      );
-
-// 热门搜索小部件
-  Container buildContainer1(str) => Container(
-        margin: EdgeInsets.only(right: 16, bottom: 16),
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.05),
-          borderRadius: BorderRadius.circular(100),
-        ),
-        padding: EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4),
-        child: Text(
-          str,
-          style: TextStyle(
-            color: Colors.white.withOpacity(0.5),
-          ),
-        ),
-      );
+Container buildHotSearch() => Container(
+      padding: EdgeInsets.only(left: 0, top: 16),
+      child: Wrap(
+        runSpacing: 8,
+        spacing: 8,
+        children: <Widget>[
+          buildContainer1('孤单心事'),
+          buildContainer1('青春住了谁'),
+          buildContainer1('Taylor Swift'),
+          buildContainer1('袁娅维远方韵律'),
+          buildContainer1('复仇者联盟'),
+          buildContainer1('bad guy'),
+        ],
+      ),
+    );
 
 // 历史搜索
-  Container buildHistoricalSearch() => Container(
-        padding: EdgeInsets.only(left: 0, top: 16),
-        child: Wrap(
-          alignment: WrapAlignment.start,
-          children: <Widget>[
-            buildContainer2('Beyond'),
-            buildContainer2('你的酒馆对我打了烊'),
-            buildContainer2('灰色轨迹'),
-            buildContainer2('大碗宽面'),
-            buildContainer2('大碗宽面'),
-            buildContainer2('大碗宽面'),
-          ],
+Container buildHistoricalSearch() => Container(
+      padding: EdgeInsets.only(left: 0, top: 16),
+      child: Wrap(
+        runSpacing: 8,
+        spacing: 8,
+        children: <Widget>[
+          buildContainer2('Beyond'),
+          buildContainer2('你的酒馆对我打了烊'),
+          buildContainer2('灰色轨迹'),
+          buildContainer2('大碗宽面'),
+          buildContainer2('大碗宽面'),
+          buildContainer2('大碗宽面'),
+        ],
+      ),
+    );
+
+// 热门搜索小部件
+Container buildContainer1(str) => Container(
+      decoration: BoxDecoration(
+        color: Color(0x101E1F2D),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      padding: EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4),
+      child: Text(
+        str,
+        style: TextStyle(
+          color: Color(0xff1E1F2D),
         ),
-      );
+      ),
+    );
 
 //历史搜索小部件
-  Container buildContainer2(str) => Container(
-        margin: EdgeInsets.only(right: 16, bottom: 16),
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.05),
-          borderRadius: BorderRadius.circular(100),
+Container buildContainer2(str) => Container(
+      decoration: BoxDecoration(
+        color: Color(0x101E1F2D),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      padding: EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4),
+      child: Text(
+        str,
+        style: TextStyle(
+          color: Color(0xff1E1F2D),
         ),
-        padding: EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4),
-        child: Text(
-          str,
-          style: TextStyle(
-            color: Colors.white.withOpacity(0.5),
-          ),
-        ),
-      );
-}
+      ),
+    );
