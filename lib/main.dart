3570946ecec634f9c7ffd4a98b11a78147c8fb69
paixs <2930118234@qq.com>
2019-06-07 04:30:41 +0800

    完善百度热搜列表的ui，实现刷新功能，增加列表项的点击扩散效果

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import './view/home_page.dart';
+import 'view/home_page.dart';
 
 void main() => runApp(MyApp());
 
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,6 +1,10 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix0;
+import 'baidu_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:gbk2utf8/gbk2utf8.dart';
+import 'splash.dart';
+import 'package:toast/toast.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,18 +12,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
-  double _fabwidth = 72;
-  double _fabheight = 56;
-  double _fabbottom = 56;
-  double _fabright = 0;
-  double _addsize = 32;
+  //百度热搜数据集
+  List<Baidu> _baidutopdata = [];
 
   //系统属性
-  Window window;
+  // Window window;
 
   @override
   void initState() {
     super.initState();
+    getBaiduTopData();
     WidgetsBinding.instance.addObserver(this);
   }
 
@@ -32,7 +34,52 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   @override
   void didChangeMetrics() {
     setState(() {
-      window = WidgetsBinding.instance.window;
+      // window = WidgetsBinding.instance.window;
+    });
+  }
+
+  //获取百度热搜数据
+  getBaiduTopData() async {
+    _baidutopdata.clear();
+    List index = List();
+    List title = List();
+    List<bool> tag1 = List();
+    List<int> tag2 = List();
+
+    http.Response response = await http.get('http://top.baidu.com/buzz?b=1');
+    String body = gbk.decode(response.bodyBytes);
+
+    //正则取标题
+    Iterable<Match> mtitle =
+        RegExp('<a class="list-title" target="_blank" href=".*?>(.*?)</a>')
+            .allMatches(body);
+    for (Match m in mtitle) {
+      title.add(m.group(1));
+    }
+    //正则取搜索指数
+    Iterable<Match> mindex =
+        RegExp('<span class="icon-.*?">(.*?)</span>').allMatches(body);
+    for (Match m in mindex) {
+      index.add(m.group(1));
+    }
+    //正则判断是否为新数据
+    String tag1body = body.replaceAll(RegExp('\\s|\n'), '');
+    Iterable<Match> mtag1 = RegExp(
+            '<aclass="list-title"target="_blank"href=".*?>.*?</a>(.*?)<ahref')
+        .allMatches(tag1body);
+    for (Match m in mtag1) {
+      tag1.add(m.group(1).isEmpty ? false : true);
+    }
+    //正则判断搜索指数上升和下降
+    Iterable<Match> mtag2 =
+        RegExp('<span class="icon-(.*?)">').allMatches(body);
+    for (Match m in mtag2) {
+      tag2.add(m.group(1) == 'rise' ? 1 : 2);
+    }
+    setState(() {
+      for (var i = 0; i < index.length; i++) {
+        _baidutopdata.add(Baidu(index[i], title[i], tag1[i], tag2[i]));
+      }
     });
   }
 
@@ -81,65 +128,137 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                       32 -
                       window.padding.top / window.devicePixelRatio,
                   width: window.physicalSize.width / window.devicePixelRatio,
-                  child: ListView(
-                    // physics: BouncingScrollPhysics(),
-                    children: <Widget>[
-                      buildListItem('1'),
-                      buildListItem('2'),
-                      buildListItem('3'),
-                      buildListItem('4'),
-                      buildListItem('5'),
-                      buildListItem('6'),
-                      buildListItem('7'),
-                      buildListItem('8'),
-                      buildListItem('9'),
-                      buildListItem('10'),
-                      buildListItem('11'),
-                      buildListItem('12'),
-                      buildListItem('13'),
-                      buildListItem('14'),
-                      buildListItem('15'),
-                      buildListItem('16'),
-                      buildListItem('17'),
-                      buildListItem('18'),
-                      buildListItem('19'),
-                      buildListItem('20'),
-                    ],
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: _baidutopdata.length,
+                    itemBuilder: (BuildContext bc, int i) {
+                      return buildListItem(
+                        (1 + i).toString(),
+                        _baidutopdata[i].title,
+                        _baidutopdata[i].index,
+                        _baidutopdata[i].tag1,
+                        _baidutopdata[i].tag2,
+                      );
+                    },
                   ),
                 )
               ],
             ),
           ),
           //小球操作按钮
-          buildPositioned()
-          // buildHotSearch(),
-          // Row(
-          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //   children: <Widget>[
-          //     Container(
-          //       alignment: Alignment.topLeft,
-          //       margin: EdgeInsets.only(left: 24, top: 24),
-          //       child: Text(
-          //         '历史搜索',
-          //         style: TextStyle(
-          //           color: Color(0xff242433),
-          //           fontSize: 16,
-          //           fontWeight: FontWeight.bold,
-          //         ),
-          //       ),
-          //     ),
-          //     Container(
-          //       margin: EdgeInsets.only(right: 24, top: 24),
-          //       child: Icon(
-          //         Icons.delete,
-          //         color: Color(0xff242433),
-          //         size: 24,
-          //       ),
-          //     )
-          //   ],
-          // ),
-          // buildHistoricalSearch(),
+          buildPositioned(),
         ],
+      ),
+    );
+  }
+
+  //列表项
+  Container buildListItem(id, title, index, tag1, tag2) {
+    return Container(
+      width: window.physicalSize.width,
+      margin: EdgeInsets.only(left: 24, right: 24, top: 12, bottom: 12),
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 24,
+            height: 24,
+            decoration: BoxDecoration(
+              color: id == '1'
+                  ? Colors.redAccent.withOpacity(0.25)
+                  : id == '2'
+                      ? Colors.orangeAccent.withOpacity(0.25)
+                      : id == '3'
+                          ? Colors.blueAccent.withOpacity(0.25)
+                          : Colors.black.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Center(
+              child: Text(
+                id,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: id == '1'
+                        ? Colors.redAccent
+                        : id == '2'
+                            ? Colors.orangeAccent
+                            : id == '3'
+                                ? Colors.blueAccent
+                                : Colors.black.withOpacity(0.5),
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          SizedBox(width: 24),
+          Container(
+            width: (window.physicalSize.width / window.devicePixelRatio) - 96,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Color(0xff242433).withOpacity(0.05),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('搜索指数 : ' + index,
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black.withOpacity(0.25))),
+                    Row(
+                      children: <Widget>[
+                        Text('新',
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: tag1 == true
+                                    ? Colors.red.withOpacity(1)
+                                    : Colors.red.withOpacity(0))),
+                        SizedBox(width: 16),
+                        Text(tag2 == 1 ? '上升↑' : '下降↓',
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: tag2 == 1 ? Colors.red : Colors.blue))
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //顶部选项卡
+  GestureDetector buildTopTab(text, opacity) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: EdgeInsets.all(8),
+        padding: EdgeInsets.only(left: 32, right: 32),
+        decoration: BoxDecoration(
+          color: Color(0xff242433).withOpacity(0.05),
+          borderRadius: BorderRadius.circular(56),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                color: Color.fromRGBO(30, 31, 45, opacity),
+                fontSize: 16,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
@@ -147,179 +266,33 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   //底部操作按钮
   Positioned buildPositioned() {
     return Positioned(
-      bottom: _fabbottom,
-      right: _fabright,
+      bottom: 28,
+      right: 28,
       child: GestureDetector(
         child: Container(
-          width: _fabwidth,
-          height: _fabheight,
+          width: 56,
+          height: 56,
           decoration: BoxDecoration(
-            color: Color(0xff242433),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(56),
-              bottomLeft: Radius.circular(56),
-            ),
-          ),
+              color: Color(0xff242433),
+              borderRadius: BorderRadius.circular(56),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff242433).withOpacity(0.5),
+                  blurRadius: 30,
+                  offset: Offset(0, 30),
+                )
+              ]),
           child: Icon(
-            Icons.add,
-            size: _addsize,
+            Icons.refresh,
+            size: 28,
             color: Colors.white,
           ),
         ),
-        onTapDown: (TapDownDetails td) {
-          setState(() {
-            _fabwidth = 80;
-          });
-        },
-        onTapUp: (TapUpDetails tu) {
-          setState(() {
-            _fabwidth = 72;
-          });
-        },
-        onVerticalDragUpdate: (DragUpdateDetails dud) {
-          setState(() {
-            _fabright = (window.physicalSize.width / window.devicePixelRatio -
-                    dud.globalPosition.dx -
-                    36) /
-                5;
-          });
-          print((window.physicalSize.width / window.devicePixelRatio -
-                      dud.globalPosition.dx)
-                  .toString() +
-              "======================");
+        onTap: () {
+          getBaiduTopData();
+          Toast.show('刷新成功！', context, backgroundColor: Colors.black);
         },
       ),
     );
   }
-
-  //列表项
-  Container buildListItem(text) {
-    return Container(
-        margin: EdgeInsets.all(16),
-        padding: EdgeInsets.all(24),
-        decoration: BoxDecoration(
-          color: Color(0xff242433).withOpacity(0.1),
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Text(
-                  text,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(width: 16),
-                Text(
-                  '华为在美提起诉讼',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-              ],
-            ),
-            Text(
-              '7922698',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black.withOpacity(0.2),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ));
-  }
 }
-
-//顶部选项卡
-GestureDetector buildTopTab(text, opacity) {
-  return GestureDetector(
-    onTap: () {},
-    child: Container(
-      margin: EdgeInsets.all(8),
-      padding: EdgeInsets.only(left: 32, right: 32),
-      decoration: BoxDecoration(
-        color: Color(0xff242433).withOpacity(0.05),
-        borderRadius: BorderRadius.circular(56),
-      ),
-      child: Center(
-        child: Text(
-          text,
-          style: TextStyle(
-              color: Color.fromRGBO(30, 31, 45, opacity),
-              fontSize: 16,
-              fontWeight: FontWeight.bold),
-        ),
-      ),
-    ),
-  );
-}
-
-// 热门搜索
-Container buildHotSearch() => Container(
-      padding: EdgeInsets.only(left: 0, top: 16),
-      child: Wrap(
-        runSpacing: 8,
-        spacing: 8,
-        children: <Widget>[
-          buildContainer1('孤单心事'),
-          buildContainer1('青春住了谁'),
-          buildContainer1('Taylor Swift'),
-          buildContainer1('袁娅维远方韵律'),
-          buildContainer1('复仇者联盟'),
-          buildContainer1('bad guy'),
-        ],
-      ),
-    );
-
-// 历史搜索
-Container buildHistoricalSearch() => Container(
-      padding: EdgeInsets.only(left: 0, top: 16),
-      child: Wrap(
-        runSpacing: 8,
-        spacing: 8,
-        children: <Widget>[
-          buildContainer2('Beyond'),
-          buildContainer2('你的酒馆对我打了烊'),
-          buildContainer2('灰色轨迹'),
-          buildContainer2('大碗宽面'),
-          buildContainer2('大碗宽面'),
-          buildContainer2('大碗宽面'),
-        ],
-      ),
-    );
-
-// 热门搜索小部件
-Container buildContainer1(str) => Container(
-      decoration: BoxDecoration(
-        color: Color(0x10242433),
-        borderRadius: BorderRadius.circular(100),
-      ),
-      padding: EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4),
-      child: Text(
-        str,
-        style: TextStyle(
-          color: Color(0xff242433),
-        ),
-      ),
-    );
-
-//历史搜索小部件
-Container buildContainer2(str) => Container(
-      decoration: BoxDecoration(
-        color: Color(0x10242433),
-        borderRadius: BorderRadius.circular(100),
-      ),
-      padding: EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4),
-      child: Text(
-        str,
-        style: TextStyle(
-          color: Color(0xff242433),
-        ),
-      ),
-    );
