23617078e9a585471939a3e2c171d2dbad7bf95f
paixs <2930118234@qq.com>
2019-06-07 04:27:19 +0800

    百度热搜实体类

---
+++ lib/view/baidu_model.dart
@@ -0,0 +1,11 @@
+class Baidu {
+  Baidu(this._index, this._title,this._tag1,this._tag2);
+  String _index;
+  String _title;
+  bool _tag1;
+  int _tag2;
+  String get index => _index;
+  String get title => _title;
+  bool get tag1 => _tag1;
+  int get tag2 => _tag2;
+}
